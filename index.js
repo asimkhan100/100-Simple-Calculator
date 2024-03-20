cd63421f7e02300db84f44a16089f771e98e1d13
asimkhan100 <164136309+asimkhan100@users.noreply.github.com>
2024-03-20 13:12:19 +0500

    This is my first assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter Second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select Operator",
+        type: "list",
+        name: "Operators",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.Operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.Operators === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.Operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.Operators === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    ("Please select operator");
+}
